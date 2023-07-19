d30002a6f08cc535e8c1d58b8c1eb8f4714c5f8e
datlqph24221 <lequocdat2312@gmail.com>
2023-07-19 21:38:59 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+module.exports = {
+    content: ["./src/**/*.{html,ts,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        plugin(function ({ addBase, theme }) {
+            addBase({
+                'h1': { fontSize: theme('fontSize.4xl') },
+                'h2': { fontSize: theme('fontSize.2xl') },
+                'h3': { fontSize: theme('fontSize.lg') },
+            })
+        })
+    ],
+}
